acf53b96163c04d91f852eb1e804b6271c4196c7
kid4211 <kid4211@naver.com>
2020-12-30 09:17:27 +0900

    react 강의 연습 파일 업로드

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { useRef, useState } from 'react';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
+
+function App() {
+    const [inputs, setInputs] = useState({
+        username: '',
+        email: '',
+    });
+    const { username, email } = inputs;
+    const onChange = (e) => {
+        const { name, value } = e.target;
+        console.log(inputs);
+        console.log({
+            ...inputs,
+            [name]: value,
+        });
+        setInputs({
+            ...inputs,
+            [name]: value,
+        });
+    };
+    const users = [
+        {
+            id: 1,
+            username: 'velopert',
+            email: 'public.velopert@gmail.com',
+        },
+        {
+            id: 2,
+            username: 'tester',
+            email: 'tester@example.com',
+        },
+        {
+            id: 3,
+            username: 'liz',
+            email: 'liz@example.com',
+        },
+    ];
+
+    const nextId = useRef(4);
+    const onCreate = () => {
+        // 나중에 구현 할 배열에 항목 추가하는 로직
+        // ...
+
+        setInputs({
+            username: '',
+            email: '',
+        });
+        nextId.current += 1;
+    };
+    return (
+        <>
+            <CreateUser
+                username={username}
+                email={email}
+                onChange={onChange}
+                onCreate={onCreate}
+            />
+            <UserList users={users} />
+        </>
+    );
+}
+
+export default App;
