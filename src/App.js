5c16ef3f09a118c66ec4ac8e7c866c5f2a2ef5dc
kid4211 <kid4211@naver.com>
2020-12-30 20:47:49 +0900

    <김진영><201230 20:47><react 수업내용 업데이트>

--- src/App.js
+++ src/App.js
@@ -7,47 +7,72 @@ function App() {
         username: '',
         email: '',
     });
-    const { username, email } = inputs;
+
+    const { username, email } = inputs; // 비구조화 할당을 통해 각 비열값을 추출해줌
+
     const onChange = (e) => {
         const { name, value } = e.target;
-        console.log(inputs);
-        console.log({
-            ...inputs,
-            [name]: value,
-        });
         setInputs({
             ...inputs,
             [name]: value,
         });
     };
-    const users = [
+    const [users, setUsers] = useState([
         {
             id: 1,
             username: 'velopert',
             email: 'public.velopert@gmail.com',
+            active: true,
         },
         {
             id: 2,
             username: 'tester',
             email: 'tester@example.com',
+            active: false,
         },
         {
             id: 3,
             username: 'liz',
             email: 'liz@example.com',
+            active: false,
         },
-    ];
+    ]);
 
     const nextId = useRef(4);
     const onCreate = () => {
-        // 나중에 구현 할 배열에 항목 추가하는 로직
-        // ...
+        /* 기존의 배열에 새항목을 추가하는 방법
+         * 1. 전개연사자
+         * 2. concat 함수 사용
+         */
+        const user = {
+            id: nextId.current,
+            username,
+            email,
+        };
+        setUsers([...users, user]);
 
         setInputs({
             username: '',
             email: '',
         });
         nextId.current += 1;
+    };
+
+    const onRemove = (id) => {
+        /* user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듦
+         * user.id 가 id 인 요소는 제외하고 배열로 추출함 */
+        setUsers(users.filter((user) => user.id !== id));
+    };
+
+    /* 불변성을 지키면서도 배열을 업데이트할때 map함수가 사용됨
+     * users의 객체들을 user로 받아서 삼항연산자를 통해 조건에 부합하는
+     * 객체만 값을 업데이트 해준다 */
+    const onToggle = (id) => {
+        setUsers(
+            users.map((user) =>
+                user.id === id ? { ...user, active: !user.active } : user
+            )
+        );
     };
     return (
         <>
@@ -57,7 +82,7 @@ function App() {
                 onChange={onChange}
                 onCreate={onCreate}
             />
-            <UserList users={users} />
+            <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
         </>
     );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,18 +1,37 @@
 import React from 'react';
 
-function User({ user }) {
+function User({ user, onRemove, onToggle }) {
+    const { username, email, id, active } = user; //비구조화 할당을 통한 변수 선언
     return (
         <div>
-            <b>{user.username}</b> <span>({user.email})</span>
+            {/* 이벤트에 함수를 걸어줄땐 반드시 익명함수형태로 선언해줘야 함
+             * 만약 onClick={onRemove(id)} 형태로 compile이 된다면
+             * 컴파일이 되는 과정중에 반복적인 삭제가 이루어짐 */}
+            {/* React에서 style={ } 기본적으로 중괄호를 통해 선언된다.
+             * 만약 스타일 요소가 배열객체라면 style={{color:'', cursor:''}}
+             * 식으로 이중 중괄호를 선언해줘야함 */}
+            <button onClick={() => onRemove(id)}>삭제</button>
+            <b
+                style={{ color: active ? 'green' : 'black', cursor: 'pointer' }}
+                onClick={() => onToggle(id)}
+            >
+                &nbsp;{username}
+            </b>{' '}
+            <span>({email})</span>
         </div>
     );
 }
 
-function UserList({ users }) {
+function UserList({ users, onRemove, onToggle }) {
     return (
         <div>
             {users.map((user) => (
-                <User user={user} key={user.id} />
+                <User
+                    user={user}
+                    key={user.id}
+                    onRemove={onRemove}
+                    onToggle={onToggle}
+                />
             ))}
         </div>
     );
